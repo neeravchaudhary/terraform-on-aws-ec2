cd49d5bce94269ab8277942db74c0e0f45497071
neeravchaudhary <neeravchaudhary022@gmail.com>
2024-12-24 14:39:23 +0100

    InputVariable_data_source_output_values

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -1,9 +1,9 @@
 # Input Variables
 # AWS Region
 variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
+  description = "Region in which AWS resource will be created"
   type = string
-  default = "us-east-1"
+  default = "eu-north-1"
 }
 
 # AWS EC2 Instance Type
--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
@@ -1,56 +1,24 @@
 # Create Security Group - SSH Traffic
 resource "aws_security_group" "vpc-ssh" {
   name        = "vpc-ssh"
-  description = "Dev VPC SSH"
-  ingress {
-    description = "Allow Port 22"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all ip and ports outbound"    
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  description = "Dev vpc ssh"
+  #vpc_id      = aws_vpc.main.id
   tags = {
     Name = "vpc-ssh"
   }
 }
 
-# Create Security Group - Web Traffic
-resource "aws_security_group" "vpc-web" {
-  name        = "vpc-web"
-  description = "Dev VPC Web"
-  ingress {
-    description = "Allow Port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "Allow Port 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  
-  egress {
-    description = "Allow all ip and ports outbound"    
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "vpc-web"
-  }
+resource "aws_vpc_security_group_ingress_rule" "Allow port 22" {
+  security_group_id = aws_security_group.vpc-ssh.id
+  cidr_ipv4         = ["0.0.0.0/0"]
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
 }
 
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_outbound" {
+  security_group_id = aws_security_group.vpc-ssh.id
+  cidr_ipv4         = ["0.0.0.0/0"]
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
